30c7ad41b3c0eed2d7e594cd87ed90142c778d3d
cjwsstrm <cjwesstrom@gmail.com>
2017-09-10 18:47:15 +0000

    Made final changes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// app.js
+/* app.js */
 
 // require and instantiate express
 const app = require('express')()
 
-// fake posts to simulate database
+// fake posts to simulate a database
 const posts = [
   {
     id: 1,
@@ -14,9 +14,9 @@ const posts = [
   {
     id: 2,
     author: 'Drake',
-    title: 'Express: starting from the Bottom',
+    title: 'Express: Starting from the Bottom',
     body: 'Blog post number 2'
-  }
+  },
   {
     id: 3,
     author: 'Emma',
@@ -36,18 +36,18 @@ app.set('view engine', 'ejs')
 
 // blog home page
 app.get('/', (req, res) => {
-  // render 'home.ejs' with the list of posts
-  res.render('home', {posts:posts})
+  // render `home.ejs` with the list of posts
+  res.render('home', { posts: posts })
 })
 
 // blog post
 app.get('/post/:id', (req, res) => {
-  // find the post in the 'posts' array
+  // find the post in the `posts` array
   const post = posts.filter((post) => {
     return post.id == req.params.id
   })[0]
 
-  // render the 'post.ejs' template with the post content
+  // render the `post.ejs` template with the post content
   res.render('post', {
     author: post.author,
     title: post.title,
@@ -55,6 +55,6 @@ app.get('/post/:id', (req, res) => {
   })
 })
 
-app.listen(8080);
+app.listen(8080)
 
-console.log('listening on port 8080');
+console.log('listening on port 8080')
